b23d03bc8bc4dba63b5f8bb723beca94ce0ba664
diannurch <diannurcahyaf>
2021-02-11 16:49:30 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,65 @@
+<?php
+/* 
+Dian Nurcahya Ningrum
+203040124
+https://github.com/diannurch/pw2021_203040124
+Pertemuan 2 - 10 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+
+<?php
+// Standar Output : mencetak sesuatu ke layar
+// echo, print
+// print_r (penulisan harus pake kurung)
+// var_dump (melihat isi variable)
+
+//  Penulisan sintaks PHP 
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variable dan Tipe Data
+// Variable 
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Dian"
+// echo "Halo, nama saya $nama";
+
+// Operator 
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y ;
+
+// penggabung string / concatenation / concat
+// . 
+// $nama_depan = "Dian Nurcahya";
+// $nama_belakang = "Ningrum";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment : Operator penugasan 
+// =, +=, -=, *=,/=, %=, .=
+// $x = 1;
+// $y -= 5;
+// echo $x;
+// $nama = "Dian Nurcahya";
+// $nama .= " ";
+// $nama . = "Ningrum"
+// echo $nama;
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, ! 
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
+?>
+
+
+
